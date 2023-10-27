02cd4ed165183da1f59c349b01ab699faa20df02
emerson-newart <emerson.art.n@hotmail.com>
2023-10-27 10:52:23 -0300

    Rest Testado

---
+++ src/main/java/com/nelioalves/cursomc/resources/WebConfig.java
@@ -0,0 +1,13 @@
+package com.nelioalves.cursomc.resources;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**");
+    }
+}
