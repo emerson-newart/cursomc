d07fd148786fcc7d837722b5df8e9e3df46531db
emerson-newart <emerson.art.n@hotmail.com>
2023-10-31 14:36:07 -0300

    Cliente, Tipo cliente, telefones e endereços

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -8,26 +8,39 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Cidade;
+import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.domain.Endereco;
+import com.nelioalves.cursomc.domain.Estado;
 import com.nelioalves.cursomc.domain.Produto;
+import com.nelioalves.cursomc.domain.enums.TipoCliente;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.repositories.ClienteRepository;
+import com.nelioalves.cursomc.repositories.EnderecoRepository;
 import com.nelioalves.cursomc.repositories.ProdutoRepository;
 import com.nelioalves.cursomc.resources.CategoriaResource;
 
 @SpringBootApplication
-//public class CursomcApplication implements CommandLineRunner {
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+//public class CursomcApplication {
 
 	//@Autowired
 	//private CategoriaRepository categoriaRepository;
 	//@Autowired
 	//private ProdutoRepository produtoRepository;
 
+	@Autowired
+	ClienteRepository clienteRepository;
+
+	@Autowired
+	EnderecoRepository enderecoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
-	//@Override
-	//public void run(String... args) throws Exception {
+	@Override
+	public void run(String... args) throws Exception {
 
 	//	Categoria cat1 = new Categoria("Informática");
 	//	Categoria cat2 = new Categoria("Escritório");
@@ -45,6 +58,20 @@ public class CursomcApplication {
 		
 	//	categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	//	produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-	//}
+
+	Estado est1 = new Estado(2, "São Paulo");
+
+	Cidade c1 = new Cidade(1,"Didadema", est1);
+
+	Cliente cli1 = new Cliente("Maria Alves Dias", "mariaalves@gmail.com", "123.456.457-20", TipoCliente.PESSOAFISICA);
+	cli1.getTelefones().addAll(Arrays.asList("11 5678-7889","11 2547-8978"));
+
+	Endereco e1 = new Endereco("Rua Flores 154","300","Apto 303", "Jardim", "12454-457", cli1, c1);
+
+	cli1.getEnderecos().addAll(Arrays.asList(e1));
+
+	clienteRepository.save(cli1);
+	enderecoRepository.save(e1);
+	}
 
 }
